444dc8ca7a6b6b32cd2de8e6fbc5518d55154745
Karim <karimwael84@gmail.com>
2021-05-02 11:27:58 +0200

    Init commit

---
+++ src/app.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+//Define paths for express config
+const publicDirPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials'); 
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath));
+
+
+app.get('',(req,res)=>{
+  res.render('index',{
+    title: 'Weather App',
+    name: 'Karim Wael'
+  });
+});
+app.get('/about',(req,res)=>{
+  res.render('about', {
+    title: 'About Me',
+    name: 'Karim Wael'
+  });
+});
+app.get('/help',(req,res)=>{
+  res.render('help', {
+    title: 'Help Page',
+    description: 'This is where you can reach for our help',
+    name: 'Karim Wael'
+  });
+});
+app.get('/weather', (req, res) => {
+  
+  if(!req.query.address){
+   return  res.send({
+     error: 'Please provide an address.'
+   });
+  }
+  const address = req.query.address;
+  geocode(address,(error, {latitude, longitude, location} = {})=>{
+  
+    if(error){
+      return res.send({error});
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if(error){
+        return res.send({error});
+      }
+      res.send({
+        location,
+        weather: forecastData,
+        address,
+      });
+    });
+    
+  });
+});
+
+app.get('/help/*', (req, res)=>{
+  res.render('404', {
+    title: '404',
+    error: 'Help article not found.',
+    name: 'Karim Wael',
+  });
+});
+app.get('*', (req, res)=>{
+  res.render('404', {
+    title: 'Error: 404',
+    error: 'Page not found.',
+    name: 'Karim Wael',
+  });
+});
+app.listen(3000, ()=>{
+  console.log('Server is up and running on port 3000');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=57e8d325bc5aa1fa099d9befefc1b357&query='+ latitude +',' + longitude;
+
+  request({url, json: true },(error,{body})=>{
+  
+    if(error){
+      callback('Unable to connect to weather servcie!', undefined);
+    }else if(body.error){
+      callback('Unable to find location', undefined);
+    }else{
+      callback(undefined,'The weather is '+ body.current.weather_descriptions[0] +'. The temperature is ' + body.current.temperature + '. It feels like ' + body.current.feelslike + ' out there. There is ' + body.current.precip + '% chance of rain.');
+    }
+    
+  });
+};
+
+module.exports = forecast;
